d6517fa10d42764de01a14cad86d1bc33724981b
vn3 <vinicosta.vn3dev@gmail.com>
2026-01-17 12:46:22 -0300

    add S3 resource and IAM minimal privilege config

--- main.tf
+++ main.tf
@@ -14,3 +14,43 @@ provider "aws" {
 variable "bucket_name" {
   default = "cloud-lab-bash-s3-vn3dev-4002-8922"
 }
+
+resource "aws_s3_bucket" "backup_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_iam_user" "backup_user" {
+  name = "s3-backup-bot"
+}
+
+resource "aws_iam_access_key" "backup_user_key" {
+  user = aws_iam_user.backup_user.name
+}
+
+resource "aws_iam_user_policy" "backup_policy" {
+  name = "S3BackupPolicy"
+  user = aws_iam_user.backup_user.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["s3:PutObject", "s3:ListBucket"]
+        Effect   = "Allow"
+        Resource = [
+          "${aws_s3_bucket.backup_bucket.arn}",
+          "${aws_s3_bucket.backup_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+output "access_key_id" {
+  value = aws_iam_access_key.backup_user_key.id
+}
+
+output "secret_access_key" {
+  value     = aws_iam_access_key.backup_user_key.secret
+  sensitive = true
+}
