94cbbbb46ce78e63b079fb01990d62fd5d9615ce
vn3 <vinicosta.vn3dev@gmail.com>
2026-01-17 22:00:46 -0300

    add variables and echoes for visibility

--- backup.sh
+++ backup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-tar -czf backup.tar.gz ./data
+BUCKET_NAME="cloud-lab-bash-s3-vn3dev-4002-8922"
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+BACKUP_FILE="backup_$TIMESTAMP.tar.gz"
+SOURCE_DIR="./data"
 
-aws s3 cp backup.tar.gz s3://cloud-lab-bash-s3-vn3dev-4002-8922/ --profile backup-bot
+echo "Starting backup process..."
+echo "Creating compressed file: $BACKUP_FILE"
+
+tar -czf $BACKUP_FILE $SOURCE_DIR
+
+echo "Uploading $BACKUP_FILE to S3 bucket: $BUCKET_NAME"
+
+aws s3 cp $BACKUP_FILE s3://$BUCKET_NAME/ --profile backup-bot
+
+echo "Backup process finished successfully!"
