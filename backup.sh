7541788b4ccb1eb313626e62f6bab7865283540a
vn3 <vinicosta.vn3dev@gmail.com>
2026-01-18 13:52:32 -0300

    add exits and

--- backup.sh
+++ backup.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+export AWS_PROFILE="backup-bot"
 BUCKET_NAME="cloud-lab-bash-s3-vn3dev-4002-8922"
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 BACKUP_FILE="backup_$TIMESTAMP.tar.gz"
 SOURCE_DIR="./data"
 
-echo "Starting backup process..."
-echo "Creating compressed file: $BACKUP_FILE"
-
+echo "Compressing files..."
 tar -czf $BACKUP_FILE $SOURCE_DIR
 
-echo "Uploading $BACKUP_FILE to S3 bucket: $BUCKET_NAME"
+if [ $? -eq 0 ]; then
+    echo "Compression successful."
+else
+    echo "Compression failed. Exiting script."
+    exit 1
+fi
 
-aws s3 cp $BACKUP_FILE s3://$BUCKET_NAME/ --profile backup-bot
+echo "Uploading to Amazon S3..."
+aws s3 cp $BACKUP_FILE s3://$BUCKET_NAME/
 
-echo "Backup process finished successfully!"
+if [ $? -eq 0 ]; then
+    echo "Upload successful."
+    
+    echo "Cleaning up local temporary files..."
+    rm $BACKUP_FILE
+    echo "Cleanup complete. Process finished."
+else
+    echo "Upload failed."
+    exit 1
+fi
